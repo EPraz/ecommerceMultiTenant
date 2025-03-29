cf77dbd932389bf225a2d8c78f39b4bd10e17338
Edwin Pile <epilepra@gmail.cm>
2025-03-29 13:32:42 -0500

    fix app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+// import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
@@ -7,7 +7,7 @@ import { SetupModule } from './setup/setup.module';
 
 @Module({
   imports: [AuthModule, SetupModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [PrismaService],
 })
 export class AppModule {}
